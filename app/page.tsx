6bd5c3dbd1a02f5ba6f7e85d4dcc4ff84e337225
spencerkasper <spener.kasper@gmail.com>
2024-09-20 11:50:38 -0500

    * Add a picture background

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
-
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div>
-          <h1 className={'text-4xl'}>Katie and Spencer&apos;s Wedding Website Placeholder</h1>
+    return (
+        <div
+            style={{backgroundImage: 'url(/background/background-4.jpg)'}}
+            className="bg-cover bg-center"
+        >
+            <div
+                className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+                <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+                    <div>
+                        <div className={'flex flex-col'}>
+                            <h1 className={'text-6xl text-black text-center font-bold'}>Katie and Spencer&apos;s Wedding
+                                Website</h1>
+                        </div>
+                        <div>
+                            <h2 className={'text-3xl text-black text-center font-bold'}>Coming Soon...</h2>
+                        </div>
+                    </div>
+                </main>
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        Made by Spencer Kasper
-      </footer>
-    </div>
-  );
+    );
 }
