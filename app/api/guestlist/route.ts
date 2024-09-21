bfdd74b18fe7bf9918e94c57a90ae292c1faf1d2
spencerkasper <spener.kasper@gmail.com>
2024-09-21 09:12:53 -0500

    * Try this?

--- app/api/guestlist/route.ts
+++ app/api/guestlist/route.ts
@@ -1,3 +1,5 @@
+import {AssumeRoleCommand, AssumeRoleResponse, STSClient} from "@aws-sdk/client-sts";
+
 export const dynamic = 'force-dynamic';
 
 import {ScanCommand} from "@aws-sdk/lib-dynamodb";
@@ -6,14 +8,34 @@ import {DynamoDBClient} from "@aws-sdk/client-dynamodb";
 
 export async function GET() {
     try {
-        const dynamo = process.env.WEDDING_ENV === 'local' ?
-            new DynamoDBClient({
+        let dynamo;
+        if(process.env.WEDDING_ENV !== 'local') {
+            console.error('Using sts for permissions.')
+            const sts = new STSClient();
+            const stsResponse: AssumeRoleResponse = await sts.send(new AssumeRoleCommand({
+                RoleArn: 'arn:aws:iam::771384749710:role/service-role/AmplifySSRLoggingRole-4e772789-d4bd-41d0-b846-607e2782af30',
+                RoleSessionName: `spencer-katie-wedding-${Date.now()}`
+            }));
+            const credentials = stsResponse ? stsResponse['Credentials'] : null;
+            const accessKey = credentials ? credentials['AccessKeyId'] : '';
+            const secretAccessKey = credentials ? credentials['SecretAccessKey'] : '';
+            const sessionToken = credentials ? credentials['SessionToken'] : '';
+            dynamo = new DynamoDBClient({
+                credentials: {
+                    accessKeyId: accessKey,
+                    secretAccessKey,
+                    ...(process.env.WEDDING_ENV !== 'local' ? {sessionToken} : {}),
+                }
+            });
+        } else {
+            console.error('Using credentials for permission.')
+            dynamo = new DynamoDBClient({
                 credentials: {
                     accessKeyId: process.env.WEDDING_AWS_ACCESS_KEY_ID || '',
                     secretAccessKey: process.env.WEDDING_AWS_SECRET_ACCESS_KEY || ''
                 }
-            }) :
-            new DynamoDBClient();
+            });
+        }
         const response = await dynamo.send(new ScanCommand({
             TableName: 'wedding_guest_list',
         }));
