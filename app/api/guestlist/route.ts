77af2fa0db4cf240552748eb29e06d4bede3d44d
spencerkasper <spener.kasper@gmail.com>
2024-09-20 22:11:25 -0500

    * Try catching server error and returning.

--- app/api/guestlist/route.ts
+++ app/api/guestlist/route.ts
@@ -5,14 +5,18 @@ import {NextResponse} from 'next/server';
 import {DynamoDBClient} from "@aws-sdk/client-dynamodb";
 
 export async function GET() {
-    const dynamo = new DynamoDBClient({
-        credentials: {
-            accessKeyId: process.env.WEDDING_AWS_ACCESS_KEY_ID || '',
-            secretAccessKey: process.env.WEDDING_AWS_SECRET_ACCESS_KEY || ''
-        }
-    });
-    const response = await dynamo.send(new ScanCommand({
-        TableName: 'wedding_guest_list',
-    }));
-    return NextResponse.json({guests: response.Items});
+    try {
+        const dynamo = new DynamoDBClient({
+            credentials: {
+                accessKeyId: process.env.WEDDING_AWS_ACCESS_KEY_ID || '',
+                secretAccessKey: process.env.WEDDING_AWS_SECRET_ACCESS_KEY || ''
+            }
+        });
+        const response = await dynamo.send(new ScanCommand({
+            TableName: 'wedding_guest_list',
+        }));
+        return NextResponse.json({guests: response.Items});
+    } catch (e) {
+        return NextResponse.json({error: e});
+    }
 }
