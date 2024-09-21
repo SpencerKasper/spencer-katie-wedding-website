e56e301bbb83a5cf44947cb7f58161b34d5cd81e
spencerkasper <spener.kasper@gmail.com>
2024-09-20 22:06:14 -0500

    * Catch error

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,21 +2,25 @@
 import HomePageClient from "@/app/home/homePageClient";
 
 export default async function AuthenticatedHomePage() {
-    const url = `${process.env.WEDDING_API_URL}/api/guestlist`;
-    console.error(`Making request to url: ${url}`);
-    const response = await fetch(url);
-    if(response && response.ok) {
-        const {guests} = await response.json();
+    try {
+        const url = `${process.env.WEDDING_API_URL}/api/guestlist`;
+        console.error(`Making request to url: ${url}`);
+        const response = await fetch(url);
+        if (response && response.ok) {
+            const {guests} = await response.json();
+            return (
+                <div className={'flex flex-row content-center w-full justify-center min-h-screen flex-wrap'}>
+                    <h1 className={'text-black text-6xl'}>Welcome to Our Wedding Website!</h1>
+                    <HomePageClient guests={guests}/>
+                </div>
+            )
+        }
+    } catch (e) {
+        console.error(e);
         return (
-            <div className={'flex flex-row content-center w-full justify-center min-h-screen flex-wrap'}>
-                <h1 className={'text-black text-6xl'}>Welcome to Our Wedding Website!</h1>
-                <HomePageClient guests={guests}/>
+            <div>
+                Could not load guests.
             </div>
         )
     }
-    return (
-        <div>
-            Could not load guests.
-        </div>
-    )
 }
