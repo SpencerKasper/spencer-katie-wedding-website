c1013a1478cf0a70b388ddf3aa280a09f53d00e5
spencerkasper <spener.kasper@gmail.com>
2024-09-20 21:38:17 -0500

    * Try this>

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,14 +1,20 @@
 'use server';
-
 import HomePageClient from "@/app/home/homePageClient";
 
 export default async function AuthenticatedHomePage() {
     const response = await fetch(`${process.env.WEDDING_API_URL}/api/guestlist`);
-    const {guests} = await response.json();
+    if(response && response.ok) {
+        const {guests} = await response.json();
+        return (
+            <div className={'flex flex-row content-center w-full justify-center min-h-screen flex-wrap'}>
+                <h1 className={'text-black text-6xl'}>Welcome to Our Wedding Website!</h1>
+                <HomePageClient guests={guests}/>
+            </div>
+        )
+    }
     return (
-        <div className={'flex flex-row content-center w-full justify-center min-h-screen flex-wrap'}>
-            <h1 className={'text-black text-6xl'}>Welcome to Our Wedding Website!</h1>
-            <HomePageClient guests={guests}/>
+        <div>
+            Could not load guests.
         </div>
     )
 }
