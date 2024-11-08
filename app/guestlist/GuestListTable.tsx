1600449e1dc8864504270cbf8eb2df1e056d22c1
spencerkasper <spener.kasper@gmail.com>
2024-11-08 11:49:10 -0600

    * Add wild logic to highlight parties

--- app/guestlist/GuestListTable.tsx
+++ app/guestlist/GuestListTable.tsx
@@ -6,23 +6,37 @@ import {useState} from "react";
 
 export function GuestListTable({guests}: { guests: Guest[] }) {
     const [addGuestOpen, setAddGuestOpen] = useState(false);
-    const guestRows = guests.map((guest, index) => {
+    const sortedGuests = guests
+        .sort((a, b) => a.partyId > b.partyId ? -1 : 1);
+    const guestRows = [];
+    let currentPartyId = '';
+    let currentColor = 'even';
+    let index = 0;
+    for(let guest of sortedGuests) {
         const address2Portion = guest.address2 && guest.address2 !== '' ? ` ${guest.address2}` : '';
         const formattedAddress = guest.address ? `${guest.address}${address2Portion}, ${guest.city}, ${guest.state} ${guest.zipCode}` : '-';
-        return (
-            <Table.Tr key={`guest-${index}`}>
+        const isEmptyAndEven = currentPartyId === '' && currentColor === 'even';
+        const isMemberOfCurrentParty = guest.partyId !== '' && guest.partyId === currentPartyId;
+        const isMemberOfCurrentPartyAndOdd = isMemberOfCurrentParty && currentColor === 'odd';
+        const dataStriped = isEmptyAndEven || isMemberOfCurrentPartyAndOdd ? 'even' : 'odd';
+        currentPartyId = guest.partyId;
+        currentColor = dataStriped;
+        index++;
+        guestRows.push(
+            <Table.Tr data-striped={dataStriped} key={`guest-${guest.firstName}-${guest.lastName}`}>
                 <Table.Td>{guest.firstName} {guest.lastName}</Table.Td>
                 <Table.Td>{guest.phoneNumber ? guest.phoneNumber : '-'}</Table.Td>
                 <Table.Td>{guest.emailAddress ? guest.emailAddress : '-'}</Table.Td>
                 <Table.Td>{formattedAddress}</Table.Td>
             </Table.Tr>
         );
-    });
+    }
+
     return (
         <Card>
             <div className={'flex justify-end'}>
                 <Button variant={'outline'} color={'green'} onClick={() => setAddGuestOpen(true)}>Add Guest</Button>
-                <AddGuestModal guests={guests} opened={addGuestOpen} onClose={() => setAddGuestOpen(false)} />
+                <AddGuestModal guests={guests} opened={addGuestOpen} onClose={() => setAddGuestOpen(false)}/>
             </div>
             <Table.ScrollContainer minWidth={500}>
                 <Table>
