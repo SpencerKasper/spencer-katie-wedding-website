644c5b8480ff633735959f39021031f5f3441b48
spencerkasper <spener.kasper@gmail.com>
2024-11-09 16:00:50 -0600

    * console error response

--- app/api/dynamodb-client.ts
+++ app/api/dynamodb-client.ts
@@ -4,17 +4,24 @@ import {DynamoDBClient} from "@aws-sdk/client-dynamodb";
 export default async function getDynamoDbClient() {
     if (process.env.NEXT_PUBLIC_WEDDING_ENV !== 'local') {
         const stsResponse = await assumeRole();
+        console.error(stsResponse);
         const credentials = stsResponse ? stsResponse['Credentials'] : null;
         const accessKey = credentials ? credentials['AccessKeyId'] : '';
         const secretAccessKey = credentials ? credentials['SecretAccessKey'] : '';
         const sessionToken = credentials ? credentials['SessionToken'] : '';
+
         return new DynamoDBClient({
             credentials: {
                 accessKeyId: accessKey,
                 secretAccessKey,
-                ...(process.env.NEXT_PUBLIC_WEDDING_ENV !== 'local' ? {sessionToken} : {}),
+                sessionToken,
             }
         });
     }
-    return new DynamoDBClient();
+    return new DynamoDBClient({
+        credentials: {
+            accessKeyId: process.env.WEDDING_AWS_ACCESS_KEY_ID || '',
+            secretAccessKey: process.env.WEDDING_AWS_SECRET_ACCESS_KEY || ''
+        }
+    });
 }
