581e8ce48e53cde128bf648ecafd1280a5dcaef2
spencerkasper <spener.kasper@gmail.com>
2024-11-09 14:06:45 -0600

    * Try this to fix

--- app/guestlist/page.tsx
+++ app/guestlist/page.tsx
@@ -1,11 +1,16 @@
+'use client';
 import axios from "axios";
 import {GuestListTable} from "@/app/guestlist/GuestListTable";
 import AdminAuthorizationRequired from "@/app/AdminAuthorizationRequired";
+import {useEffect, useState} from "react";
 
-export default async function GuestListPage() {
-    const url = `${process.env.NEXT_PUBLIC_WEDDING_API_URL}/api/guestlist`;
-    const guestListResponse = await axios.get(url);
-    const {guests} = guestListResponse.data;
+export default function GuestListPage() {
+    const [guests, setGuests] = useState([]);
+    useEffect(() => {
+        const url = `${process.env.NEXT_PUBLIC_WEDDING_API_URL}/api/guestlist`;
+        axios.get(url)
+            .then(guestListResponse => setGuests(guestListResponse.data.guests));
+    }, []);
     return (
         <AdminAuthorizationRequired>
             <div className={'p-4 md:px-8'}>
