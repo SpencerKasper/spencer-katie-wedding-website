d886e7c0884e63644b227240e2badf7865cb2e2f
spencerkasper <spener.kasper@gmail.com>
2024-12-01 19:57:03 -0600

    * More organization.

--- app/engagement-photos/page.tsx
+++ app/engagement-photos/page.tsx
@@ -8,15 +8,15 @@ import useLoggedInGuest from "@/app/hooks/useLoggedInGuest";
 export default function EngagementPhotosPage() {
     return (
         <div className={'flex flex-col justify-center items-center min-h-screen text-white'}>
+            <EngagementPhotos />
+            <div className={'flex p-8 justify-center items-center'}>
+                <a className={'underline'} target={'_blank'} href={'https://wdavisphoto.com/'}>Photos by Wendy Davis Photography</a>
+            </div>
             <div className={'flex flex-row px-16 py-4'}>
                 <p className={'font-bold'}>
                     * You can use the arrow keys on your keyboard or swipe on your phone to go through the photos. To
                     view the full quality images in full screen, just click on the picture.
                 </p>
-            </div>
-            <EngagementPhotos />
-            <div className={'flex p-8 justify-center items-center'}>
-                <a className={'underline'} target={'_blank'} href={'https://wdavisphoto.com/'}>Photos by Wendy Davis Photography</a>
             </div>
         </div>
     )
