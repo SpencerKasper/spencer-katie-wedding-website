b286d9e8609fa74e645215e6ffa522f53284ff8a
spencerkasper <spener.kasper@gmail.com>
2025-02-19 19:33:14 -0600

    * Add days until

--- app/KatieAndSpencersWeddingTitle.tsx
+++ app/KatieAndSpencersWeddingTitle.tsx
@@ -1,8 +1,19 @@
 import {OverrideFont} from "@/app/components/OverrideFont";
 
 const KatieAndSpencersWeddingTitle = () => {
+    const getDaysBetweenDates = (date1, date2) => {
+        const timeDiff = Math.abs(date2.getTime() - date1.getTime());
+        return Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+    };
+
+    const today = new Date();
+    today.setHours(0,0,0,0);
+    const date2 = new Date('2025-10-11');
+
+    const daysBetween = getDaysBetweenDates(today, date2);
+    console.log(daysBetween);
     return (
-        <div className={'text-white flex flex-col justify-center items-center'}>
+        <div className={'text-white flex flex-col justify-center items-center gap-4'}>
             <div className={'py-8'}>
                 <OverrideFont>
                     <div className={'text-center'}>
@@ -13,6 +24,9 @@ const KatieAndSpencersWeddingTitle = () => {
             </div>
             <div>
                 <p className={'text-4xl sm:text-5xl'}>10.11.2025</p>
+            </div>
+            <div>
+                <p className={'text-2xl sm:text-3xl'}>{`${daysBetween} Days To Go`}</p>
             </div>
         </div>
     )
--- app/SaveTheDate.tsx
+++ app/SaveTheDate.tsx
@@ -3,18 +3,29 @@ import {OverrideFont} from "@/app/components/OverrideFont";
 import KatieAndSpencersWeddingTitle from "@/app/KatieAndSpencersWeddingTitle";
 import EmailModal from "@/app/EmailModal";
 import useLoggedInGuest from "@/app/hooks/useLoggedInGuest";
+import Image from "next/image";
+import {Popover, Tooltip} from "@mantine/core";
 
 const SaveTheDate = () => {
     const googleCalendarUrl = `https://calendar.google.com/calendar/render?action=TEMPLATE&text=Katie+and+Spencer's+Wedding&details=Spencer Kasper and Katie Riek are getting married!&dates=20251011T160000/20251011T230000&ctz=America/Chicago&location=3500 Midwest Road, Oak Brook, IL 60523 USA`
     return (
         <div className={'text-white full-w flex flex-col justify-center items-center text-center flex-wrap p-4 sm:p-8'}>
-            <KatieAndSpencersWeddingTitle />
+            <KatieAndSpencersWeddingTitle/>
             <div className={'py-4'}>
-                <a
-                    className={'text-xl sm:text-2xl underline cursor-pointer'}
-                    href={googleCalendarUrl}
-                    target={'_blank'}
-                >Save the Date and Add to Your Google Calendar</a>
+                <Tooltip label={'Save to Google Calendar'}>
+                    <a
+                        href={googleCalendarUrl}
+                        target={'_blank'}
+                    >
+                        <Image
+                            width={64}
+                            height={64}
+                            src={'https://upload.wikimedia.org/wikipedia/commons/a/a5/Google_Calendar_icon_%282020%29.svg'}
+                            className={'text-xl sm:text-2xl underline cursor-pointer'}
+                            alt={'google-calendar-logo'}
+                        />
+                    </a>
+                </Tooltip>
             </div>
         </div>
     );
