4e0d522e52f663f188f0324c41f44278b56500f7
spencerkasper <spener.kasper@gmail.com>
2025-04-11 12:48:26 -0500

    * Fix build

--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -1,8 +1,18 @@
 'use client';
 import {useRouter} from "next/navigation";
+import {useEffect} from "react";
 
 export default function Logout() {
     const router = useRouter();
-    localStorage.clear();
-    router.push('/');
+
+    useEffect(() => {
+        localStorage.clear();
+        router.push('/');
+    }, [router]);
+
+    return (
+        <div>
+            <p className={'text-center text-white w-full'}>Logging out...</p>
+        </div>
+    )
 }
