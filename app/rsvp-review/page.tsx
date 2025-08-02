cf1df6c55109fb16f35f626942b43baf1895c6e1
spencerkasper <spener.kasper@gmail.com>
2025-08-02 14:03:48 -0500

    * Add more to rsvp-review

--- app/rsvp-review/page.tsx
+++ app/rsvp-review/page.tsx
@@ -13,22 +13,41 @@ export default function RSVPReviewPage() {
     const [rsvps, setRsvps] = useState([] as RSVP[]);
 
     useEffect(() => {
-        axios.get(`${process.env.NEXT_PUBLIC_WEDDING_API_URL}/api/guestlist`)
-            .then(guestListResponse => {
-                setGuestList(guestListResponse.data.guests);
-                axios.get(`${process.env.NEXT_PUBLIC_WEDDING_API_URL}/api/rsvp`)
-                    .then(rsvpsResponse => {
-                        setRsvps(rsvpsResponse.data.rsvps);
-                    });
-            });
+        Promise.all([
+            axios.get(`${process.env.NEXT_PUBLIC_WEDDING_API_URL}/api/guestlist`)
+                .then(guestListResponse => {
+                    const guests = guestListResponse.data.guests as Guest[];
+                    setGuestList(guests.sort((a, b) => a.partyId !== '' ? -1 : 1));
+                }),
+            axios.get(`${process.env.NEXT_PUBLIC_WEDDING_API_URL}/api/rsvp`)
+                .then(rsvpsResponse => {
+                    setRsvps(rsvpsResponse.data.rsvps);
+                }),
+        ]).then();
+
     }, []);
-    const partyNumbersMap = guestList.reduce((acc, curr) =>
-        ({
-            ...acc,
-            [curr.partyId]: acc.hasOwnProperty(curr.partyId) ? acc[curr.partyId] : Object.values(acc).length === 0 ? 1 : Math.max(...(Object.values(acc) as number[])) + 1,
-        }), {});
-    const rows = rsvps
-        .sort((a, b) => partyNumbersMap[a.guest.partyId] < partyNumbersMap[b.guest.partyId] ? -1 : 1)
+    const partyNumbersMap = guestList
+        .filter(g => rsvps.filter(r => r.guest.guestId === g.guestId).length)
+        .reduce((acc, curr) =>
+            ({
+                ...acc,
+                [curr.partyId]: acc.hasOwnProperty(curr.partyId) ? acc[curr.partyId] : Object.values(acc).length === 0 ? 1 : Math.max(...(Object.values(acc) as number[])) + 1,
+            }), {});
+    const rows = rsvps.sort((a, b) => {
+        const partyIdA = a.guest.partyId || '';
+        const partyIdB = b.guest.partyId || '';
+
+        if (partyIdA === '') return 1;
+        if (partyIdB === '') return -1;
+
+        const partyNumberA = Number(partyNumbersMap[partyIdA] ?? Infinity);
+        const partyNumberB = Number(partyNumbersMap[partyIdB] ?? Infinity);
+
+        if (isNaN(partyNumberA)) return 1;
+        if (isNaN(partyNumberB)) return -1;
+
+        return partyNumberA - partyNumberB;
+    })
         .map((x, index) => {
             const guest = x.guest;
             const partyNumber = x.guest.partyId ? partyNumbersMap[x.guest.partyId] : -1;
@@ -49,19 +68,45 @@ export default function RSVPReviewPage() {
             );
         })
 
+    const dinnerBreakdownMap = rsvps
+        .filter(r => r.isAttending)
+        .reduce((acc, curr) => ({
+            ...acc,
+            [curr.dinnerChoice]: acc.hasOwnProperty(curr.dinnerChoice) ? acc[curr.dinnerChoice] + 1 : 1
+        }), {});
     return (
         <AdminAuthorizationRequired>
             <div className={'p-2 md:p-8'}>
                 <Card>
-                    <div className={'flex justify-between'}>
-                        <a className={'underline cursor-pointer'}
-                           href={`${process.env.NEXT_PUBLIC_WEDDING_API_URL}/api/guestlist-xlsx`}
-                        >
-                            Download Excel Export
-                        </a>
-                        <div className={'flex gap-8 items-center'}>
-                            <p className={'font-bold'}>Total Guests Attending:</p>
-                            <p> {rsvps.filter(r => r.isAttending).length}</p>
+                    <div className={'flex justify-between flex-wrap gap-4'}>
+                        <div>
+                            <a className={'underline cursor-pointer'}
+                               href={`${process.env.NEXT_PUBLIC_WEDDING_API_URL}/api/guestlist-xlsx`}
+                            >
+                                Download Excel Export
+                            </a>
+                        </div>
+                        <div>
+                            <div className={'flex gap-8 items-center'}>
+                                <p className={'font-bold'}>Total Attending:</p>
+                                <p> {rsvps.filter(r => r.isAttending).length}</p>
+                            </div>
+                            <div className={'flex gap-8 items-center'}>
+                                <p className={'font-bold'}>Total Not Attending:</p>
+                                <p> {rsvps.filter(r => !r.isAttending).length}</p>
+                            </div>
+                        </div>
+                        <div className={'flex-col gap-8 items-center'}>
+                            <p className={'font-bold'}>Dinner Breakdown:</p>
+                            <div>
+                                <p>
+                                    {Object.keys(dinnerBreakdownMap)
+                                        .sort((a, b) => dinnerBreakdownMap[a] > dinnerBreakdownMap[b] ? -1 : 1)
+                                        .map((d, index) => (
+                                            <p key={`dinner-choice-summary-${index}`}>{d} - {dinnerBreakdownMap[d]}</p>
+                                        ))}
+                                </p>
+                            </div>
                         </div>
                     </div>
                     <Divider my={'md'}/>
